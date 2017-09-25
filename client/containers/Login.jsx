f220cbdeaad4abab25cec17f4114e75e98d8e99a
Kamyl <kamillcybulski@gmail.com>
2017-09-25 21:58:59 +0200

    Add very basic UI to Login and Signup components

--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -2,17 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import logUserIn from '../actions/user-actions';
 
 
 class Login extends React.Component {
   /**
+   * username -> store the current value of username text field
+   * password -> store the current value of password text field
    * @constructor
    */
   constructor() {
     super();
-    this.state = { msg: 'Want to log in?' };
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  /**
+   * handlePasswordChange
+   * @param {object} e Keyboard event object
+   * Synchronise the values of this.state.password and password textfield
+   * @returns {undefined}
+   */
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  }
+
+  /**
+   * handleUsernameChange
+   * @param {object} e Keyboard event object
+   * Synchronise the values of this.state.username and username textfield
+   * @returns {undefined}
+   */
+  handleUsernameChange = (e) => {
+    this.setState({ username: e.target.value });
   }
 
   /**
@@ -20,10 +46,20 @@ class Login extends React.Component {
    */
   render() {
     return (
-      <div>
-        {this.state.msg}
+      <div className="fullwidth flex-column">
+        <TextField
+          hintText="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        <TextField
+          hintText="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
         <RaisedButton
           label="Log in!"
+          primary
           onClick={() => {
             this.props.logIn('Janek', 'human@fromhell.stuff', 'wroclaw');
           }}
--- client/containers/Signup.jsx
+++ client/containers/Signup.jsx
@@ -1,7 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-const Signup = () => (
-  <div>This is Signup page</div>
-);
+class Signup extends React.Component {
+  /**
+   * username, email, password -> control the values od relevant textfields
+   * @constructor
+   */
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+    };
+  }
+
+  /**
+   * handleUsernameChange
+   * @param {object} e Keyboard event object
+   * Synchronise the value of this.state.username with username textfield
+   * @returns {undefined}
+   */
+  handleUsernameChange = (e) => {
+    this.setState({ username: e.target.value });
+  }
+
+  /**
+   * handleEmailChange
+   * @param {object} e Keyboard event object
+   * Synchronise the value of this.state.email with email textfield
+   * @returns {undefined}
+   */
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value });
+  }
+
+  /**
+   * handlePasswordChange
+   * @param {object} e Keyboard event object
+   * Synchronise the value of this.state.password with password textfield
+   * @returns {undefined}
+   */
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  }
+
+  /**
+   * @returns {object} React element
+   */
+  render() {
+    return (
+      <div className="fullwitdh flex-column">
+        <TextField
+          hintText="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        <TextField
+          hintText="email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <TextField
+          hintText="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <RaisedButton
+          label="Sign up!"
+          primary
+        />
+      </div>
+    );
+  }
+}
 
 export default Signup;
