3d481a250c0aa930fc44b2d01449785548b0ebdd
Kamyl <kamillcybulski@gmail.com>
2017-10-04 19:02:58 +0200

    Update tests for user reducer

--- test/user-reducer.test.js
+++ test/user-reducer.test.js
@@ -2,12 +2,23 @@ import reducer from '../client/reducers/userReducer';
 
 
 describe('LOG_IN action:', () => {
-  const action = {
+  const action1 = {
     type: 'LOG_IN',
     payload: {
       name: 'Marek',
       email: 'ziuziu@ziu.pl',
       location: 'warsaw',
+      barsToAttend: [],
+    },
+  };
+
+  const action2 = {
+    type: 'LOG_IN',
+    payload: {
+      name: 'Marek',
+      email: 'ziuziu@ziu.pl',
+      location: 'warsaw',
+      barsToAttend: ['bar1-id', 'bar2-id'],
     },
   };
 
@@ -17,16 +28,34 @@ describe('LOG_IN action:', () => {
     name: '',
     email: '',
     location: '',
+    barsToAttend: [],
   };
 
-  it('works as intended', () => {
-    expect(reducer(state, action)).toEqual(expect.objectContaining({
+  it('Logs in user with no bars to attend', () => {
+    const newState = reducer(state, action1);
+
+    expect(newState).toEqual(expect.objectContaining({
       checkedInDB: true,
       isLoggedIn: true,
       name: 'Marek',
       email: 'ziuziu@ziu.pl',
       location: 'warsaw',
     }));
+    expect(newState.barsToAttend).toHaveLength(0);
+  });
+
+  it('Logs in user with 2 bars to attend', () => {
+    const newState = reducer(state, action2);
+
+    expect(newState).toEqual(expect.objectContaining({
+      checkedInDB: true,
+      isLoggedIn: true,
+      name: 'Marek',
+      email: 'ziuziu@ziu.pl',
+      location: 'warsaw',
+      barsToAttend: expect.arrayContaining(['bar1-id', 'bar2-id']),
+    }));
+    expect(newState.barsToAttend).toHaveLength(2);
   });
 });
 
@@ -40,16 +69,98 @@ describe('LOG_OUT action:', () => {
     name: 'Marek',
     email: 'xxxxx',
     location: 'bumretch',
+    barsToAttend: ['bar1-id', 'bar2-id'],
   };
 
-  it('works as intended', () => {
-    expect(reducer(state, action)).toEqual(expect.objectContaining({
+  it('Logs user out', () => {
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual(expect.objectContaining({
       checkedInDB: true,
       isLoggedIn: false,
       name: '',
       email: '',
       location: '',
     }));
+    expect(newState.barsToAttend).toHaveLength(0);
+  });
+});
+
+describe('MARK_USER_CHECKED action:', () => {
+  const action = { type: 'MARK_USER_CHECKED' };
+
+  const state = {
+    checkedInDB: false,
+    isLoggedIn: false,
+    name: '',
+    email: '',
+    location: '',
+    barsToAttend: [],
+  };
+
+  it('Only marks the user as checked in DB', () => {
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual(expect.objectContaining({
+      checkedInDB: true,
+      isLoggedIn: false,
+      name: '',
+      email: '',
+      location: '',
+    }));
+    expect(newState.barsToAttend).toHaveLength(0);
+  });
+});
+
+describe('UPDATE_LOCATION action:', () => {
+  const state1 = {
+    checkedInDB: true,
+    isLoggedIn: true,
+    name: 'Marek',
+    email: 'xxxxx',
+    location: 'bumretch',
+    barsToAttend: ['bar1-id', 'bar2-id'],
+  };
+
+  const state2 = {
+    checkedInDB: true,
+    isLoggedIn: true,
+    name: 'Marek',
+    email: 'aaaaaa',
+    location: '',
+    barsToAttend: [],
+  };
+
+  const action = {
+    type: 'UPDATE_LOCATION',
+    payload: 'gdynia',
+  };
+
+  it('Replaces the old location', () => {
+    const newState = reducer(state1, action);
+
+    expect(newState).toEqual(expect.objectContaining({
+      checkedInDB: true,
+      isLoggedIn: true,
+      name: 'Marek',
+      email: 'xxxxx',
+      location: 'gdynia',
+      barsToAttend: expect.arrayContaining(['bar1-id', 'bar2-id']),
+    }));
+    expect(newState.barsToAttend).toHaveLength(2);
+  });
+
+  it('Assigns a new location if user didn\'t have one', () => {
+    const newState = reducer(state2, action);
+
+    expect(newState).toEqual(expect.objectContaining({
+      checkedInDB: true,
+      isLoggedIn: true,
+      name: 'Marek',
+      email: 'aaaaaa',
+      location: 'gdynia',
+    }));
+    expect(newState.barsToAttend).toHaveLength(0);
   });
 });
 
