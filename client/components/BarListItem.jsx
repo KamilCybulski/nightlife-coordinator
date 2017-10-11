0276e51ba170e2f9f1afea73380bfa42fc8f1d35
Kamyl <kamillcybulski@gmail.com>
2017-10-11 19:51:29 +0200

    Fix the text related to number of attendants in BarListItem

--- client/components/BarListItem.jsx
+++ client/components/BarListItem.jsx
@@ -3,6 +3,25 @@ import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 
+/**
+ * insertProperText
+ * @param {number} attendants Number of attendants
+ * @returns {string} Proper description of the number of attendants going to
+ * a given bar
+ */
+const insertProperText = (attendants) => {
+  switch (attendants) {
+    case 0:
+      return 'Nobody is going there.';
+
+    case 1:
+      return 'Only one person is going there.';
+
+    default:
+      return `${attendants} people are going there.`;
+  }
+};
+
 const BarListItem = ({ name, rating, attendants, btnLabel, btnFunc, url }) => (
   <Paper
     zDepth={2}
@@ -12,11 +31,14 @@ const BarListItem = ({ name, rating, attendants, btnLabel, btnFunc, url }) => (
       {name}
     </h3>
     <p>This place&#39;s rating is {rating}.</p>
-    <p>{attendants} people are going there.</p>
-    <p><a href={url}>See the website</a></p>
+    <p>{insertProperText(attendants)}</p>
+    <p><a href={url} className="undecorated-link">
+      See their website
+    </a></p>
     {!btnLabel && !btnFunc
       ? null
       : <RaisedButton
+        className="width150"
         label={btnLabel}
         onClick={btnFunc}
         primary
